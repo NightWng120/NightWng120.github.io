670a039883bd32d233242422a1eaea144f8929f8
NightWng120 <treverholmes9@gmail.com>
2024-08-18 08:16:50 -0400

    added anchor tag

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,17 @@ function main(){
 	const userName = document.createElement("INPUT");
 	const email = document.createElement("INPUT");
 	const password = document.createElement("INPUT");
+	const anchor = document.createElement("a");
+	const linebreak = document.createElement("br");
+	const div = document.createElement("div");
+	const style = document.createElement("style");
+	style.textContent = `
+	.bottom {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+    }`;
+	document.head.appendChild(style);
 
 	userName.setAttribute('type', 'text');
 	userName.setAttribute('id', 'username');
@@ -24,11 +35,20 @@ function main(){
 	password.setAttribute('maxlength', '12');
 	password.setAttribute('required', true);
 	document.body.appendChild(password);
+
+	div.id = "bottom";
+	anchor.href = "game.html";
+	anchor.textContent = "Mastermind";
+	div.appendChild(anchor);
+	document.body.appendChild(div);
+
+
+
 }
 
 newpage = () => {
 	document.body.innerHTML = '';
-	document.body.innerHTML = "<h1>Login/Signup Page</h1> <br>";
+	document.body.innerHTML = `<h1>Login/Signup Page</h1> <br>`;
 
 }
 
